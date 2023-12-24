ab2f38c7c55287098221258da510ce662ba4123d
Md Zobayer Hossain Mamun <zobayerhossainmamun@gmail.com>
2023-12-24 19:49:31 +0600

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const user = {
+  email: 'admin@gmail.com',
+  password: 12345678
+}
+const options = {
+  expiresIn: '1h', // Token expiration time
+  issuer: 'your-app-name', // Token issuer
+};
+const secretKey = 'test-secret';
+
+router.get('/', function (req, res, next) {
+  res.status(200).json({ message: 'Hello World!' });
+});
+
+router.post('/login', function (req, res, next) {
+  const data = req.body;
+
+  if (data.email !== user.email) {
+    return res.status(203).json({ message: 'Email does not exist.' });
+  }
+
+  if (data.password !== user.password) {
+    return res.status(203).json({ message: 'Password is wrong.' });
+  }
+
+  const token = jwt.sign(user, secretKey, options);
+  res.status(200).json({
+    accessToken: token
+  })
 });
 
 module.exports = router;
